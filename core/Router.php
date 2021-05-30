7260b23646aef6e76d9630d6b0131a4fcecadd19
Kallyas <kallyasmedia@gmail.com>
2021-05-30 18:04:09 +0300

    Add layouts and implement content replacement on render view

--- core/Router.php
+++ core/Router.php
@@ -5,11 +5,13 @@ namespace app\core;
 class Router
 {
     public Request $request;
+    public Response $response;
     protected array $routes = [];
 
-    public function __construct($request)
+    public function __construct(Request $request, Response $response)
     {
         $this->request = $request;
+        $this->response = $response;
     }
 
 
@@ -25,13 +27,35 @@ class Router
         $callback = $this->routes[$method][$path] ?? false;
 
         if ($callback === false) {
+            $this->response->setStatusCode(404);
             return "Not Found!";
         }
 
         if (is_string($callback)) {
-            return $this->renderView();
+            return $this->renderView($callback);
         }
 
         return call_user_func($callback);
+    }
+
+    public function renderView($view)
+    {
+        $layoutContent = $this->layoutContent();
+        $viewContent = $this->renderOnlyView($view);
+        return str_replace("{{content}}", $viewContent, $layoutContent);
+    }
+
+    protected function layoutContent()
+    {
+        ob_start();
+        include_once Application::$ROOT_DIR . "/views/layouts/main.php";
+        return ob_get_clean();
+    }
+
+    protected function renderOnlyView($view)
+    {
+        ob_start();
+        include_once Application::$ROOT_DIR . "/views/$view.php";
+        return ob_get_clean();
     }
 }
