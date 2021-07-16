3cd673a6420b6d8c3d8ad86944ca198daac1c7a2
Kallyas <kallyasmedia@gmail.com>
2021-07-16 19:00:12 +0300

    restructure entry point

--- public/index.php
+++ public/index.php
@@ -2,24 +2,36 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-use app\controllers\AuthController;
+use app\controllers\AboutController;
 use app\controllers\SiteController;
 use app\core\Application;
 
-$app = new Application(dirname(__DIR__));
+require_once __DIR__ . '/../vendor/autoload.php';
+$dotenv = \Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->load();
+$config = [
+    'userClass' => \app\models\User::class,
+    'db' => [
+        'dsn' => $_ENV['DB_DSN'],
+        'user' => $_ENV['DB_USER'],
+        'password' => $_ENV['DB_PASSWORD'],
+    ]
+];
+
+$app = new Application(dirname(__DIR__), $config);
+
+$app->on(Application::EVENT_BEFORE_REQUEST, function () {
+    echo "Before request from second installation";
+});
 
 $app->router->get('/', [SiteController::class, 'home']);
-
+$app->router->get('/register', [SiteController::class, 'register']);
+$app->router->post('/register', [SiteController::class, 'register']);
+$app->router->get('/login', [SiteController::class, 'login']);
+$app->router->post('/login', [SiteController::class, 'login']);
+$app->router->get('/logout', [SiteController::class, 'logout']);
 $app->router->get('/contact', [SiteController::class, 'contact']);
-
-$app->router->post('/contact', [SiteController::class, 'handleContact']);
-
-$app->router->get('/login', [AuthController::class, 'login']);
-
-$app->router->post('/login', [AuthController::class, 'login']);
-
-$app->router->get('/register', [AuthController::class, 'register']);
-
-$app->router->post('/register', [AuthController::class, 'register']);
+$app->router->get('/about', [AboutController::class, 'index']);
+$app->router->get('/profile', [SiteController::class, 'profile']);
 
 $app->run();
